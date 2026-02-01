6ba9bac0049cdd3a7c5e5af3ffc4073dc34caeaf
Antoine-Bouclier <antoinebouclier1@gmail.com>
2026-02-01 10:47:41 +0100

    Ex02: Init

---
+++ ex02/inc/Array.hpp
@@ -0,0 +1,37 @@
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <iostream>
+
+template <typename T>
+class Array
+{
+	private:
+		T				*_array;
+		unsigned int	_size;
+	public:
+		/* -- Constructors -- */
+		Array();
+		Array(unsigned int size);
+		Array(Array const &copy);
+
+		/* -- Operators -- */
+		Array	&operator=(Array const &copy);
+		T		&operator[](unsigned int i);
+		T		&operator[](unsigned int i) const;
+
+		/* -- Member function -- */
+		unsigned int	size() const;
+
+		/* -- Destructor -- */
+		~Array();
+
+		/* -- Exceptions -- */
+		class OutOfBoundsException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+#endif
