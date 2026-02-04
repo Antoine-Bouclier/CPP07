ab6ab9d534de2a0d3badc5858ec80b877c81e69b
Antoine <antoinebouclier1@gmail.com>
2026-02-04 12:05:52 +0100

    Ex01: main.cpp Done.

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,6 +1,54 @@
 #include "iter.hpp"
+#include <iostream>
 
-int	main(void)
+template< typename T >
+void print( T& x )
 {
-	return (0);
+	std::cout << x << ' ';
+	return;
+}
+
+void	increment(int &x)
+{
+	x++;
+}
+
+void	toUpper(char &c)
+{
+	c = (char)toupper(c);
+}
+
+int main()
+{
+	int intArray[] = {1, 2, 3, 4, 5};
+	char charArray[] = {'a', 'b', 'c', 'd'};
+
+	std::cout << "Array before incrementation : ";
+	iter(intArray, 5, print<int>);
+	std::cout << std::endl;
+
+	iter(intArray, 5, increment);
+
+	std::cout << "Array after incrementation : ";
+	iter(intArray, 5, print<int>);
+	std::cout << std::endl;
+
+	std::cout << "Array of char before toUpper : ";
+	iter(charArray, 4, print<char>);
+	std::cout << std::endl;
+
+	iter(charArray, 4, toUpper);
+
+	std::cout << "Array of char after toUpper : ";
+	iter(charArray, 4, print<char>);
+	std::cout << std::endl;
+
+	std::cout << "TEST const" << std::endl;
+	iter(charArray, 4, print<const char>);
+	iter(intArray, 4, print<const int>);
+
+	// std::cout << "TEST third parameter is not a function" << std::endl;
+	// iter(charArray, 4, 4);
+
+	return 0;
 }
