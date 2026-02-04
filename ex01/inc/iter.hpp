5c9344f7a1042effd14b3ee157c6728150791f0c
Antoine <antoinebouclier1@gmail.com>
2026-02-04 12:06:22 +0100

    iter accept const in third param

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -1,12 +1,11 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T> void iter(T *array, const unsigned int length, void (*f)(T &))
+template <typename T, typename F>
+void iter(T* array, unsigned int length, F f)
 {
-	for (int i = 0; i < length; ++i)
-	{
+	for (unsigned int i = 0; i < length; ++i)
 		f(array[i]);
-	}
 }
 
 #endif
